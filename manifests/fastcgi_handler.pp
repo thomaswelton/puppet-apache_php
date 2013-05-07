64bdd3676ea046c3dc2d47d03cb2fa796b5cd35e
Florian Weber <f.weber@digi-info.de>
2013-05-07 11:01:27 +0200

    Fixed order of config files.

--- manifests/fastcgi_handler.pp
+++ manifests/fastcgi_handler.pp
@@ -1,5 +1,5 @@
 define apache_php::fastcgi_handler ($php_version, $ensure = 'present') {
-  file { "${apache::config::configdir}/other/fastcgi-${name}.conf":
+  file { "${apache::config::configdir}/other/10-fastcgi-${name}.conf":
     ensure  => $ensure,
     content => template('apache_php/fastcgi_handler.conf.erb'),
   }
