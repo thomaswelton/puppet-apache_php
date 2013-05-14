2f58aa91e82a160a7acdc7f01504bfa082d08ff2
Florian Weber <f.weber@digi-info.de>
2013-05-14 22:54:52 +0200

    Require apache::config.

--- manifests/fastcgi_handler.pp
+++ manifests/fastcgi_handler.pp
@@ -1,4 +1,6 @@
 define apache_php::fastcgi_handler ($php_version, $ensure = 'present') {
+  require apache::config
+
   file { "${apache::config::configdir}/other/10-fastcgi-${name}.conf":
     ensure  => $ensure,
     content => template('apache_php/fastcgi_handler.conf.erb'),
